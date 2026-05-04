81b2908a5ca32ddb1a01d83b926d08ecdc251f45
nsa2412 <ale.nicolas.salomon@gmail.com>
2026-05-04 17:52:31 -0300

    problema 06

---
+++ Problema6/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema6
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string value)
+        {
+            if (value == null)
+            {
+                return "SIN-CODIGO";
+            }
+
+            return value.Trim().ToUpper().Replace(" ", "-");
+        }
+    }
+}
