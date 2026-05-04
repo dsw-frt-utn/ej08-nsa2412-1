cf18af2d091b60edca7a74e5866e86348c6f7764
nsa2412 <ale.nicolas.salomon@gmail.com>
2026-05-04 17:49:53 -0300

    problema 03

---
+++ Problema3/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema3
+{
+    public class Product
+    {
+        private string _Description = "";
+
+        public Product (string desription)
+        {
+            _Description = desription;
+        }
+        public void SetDescription (string description)
+        {
+            _Description = description;
+        }
+        public string GetDescription()
+        {
+            return _Description;
+        }
+
+        public void ModificarDescripcion(string nuevaDescripcion)
+        {
+            _Description = nuevaDescripcion;
+        }
+    }
+}
